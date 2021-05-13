f35e58d0a5593688ece232e4fab517514f804710
Timothy Kuebler <timothy.j.kuebler@gmail.com>
2021-05-13 17:26:32 -0400

    added utils folder

--- userdirectory/src/components/Table/Table.js
+++ userdirectory/src/components/Table/Table.js
@@ -3,7 +3,11 @@ import React from 'react';
 function Table(props) {
     return (
         <table>
-            
+        <thread>
+            <tr>
+                {}
+            </tr>
+        </thread>
         </table>
     )
 }
