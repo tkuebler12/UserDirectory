e17e6029b26ea4ce4d78a8a980ac8f61730b1749
Timothy Kuebler <timothy.j.kuebler@gmail.com>
2021-05-13 17:06:47 -0400

    deleted unused files

--- userdirectory/pages/Search.js
+++
@@ -1,4 +0,0 @@
-import React, { Component } from "react";
-import API from "../utils/API";
-import SearchForm from "../components/SearchForm";
-import SearchResults from "../components/SearchResults";
---
+++ userdirectory/src/components/Table/Table.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Table(props) {
+    return (
+        <table>
+            
+        </table>
+    )
+}
+
+export default Table;
